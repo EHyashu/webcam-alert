99c354792a099452cb6681271b01ffd95463f6ed
Aryan Khatri <khatriaryan880@gmail.com>
2025-11-05 19:36:26 +0530

    Initial commit - webcam alert project

---
+++ venv/emailing.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+PASSWORD =""
+SENDER="khatriaryan880@gmail.com"
+RECEIVER="khatriaryan880@gmail.com"
+
+def send_email(image_path):
+    email_message= EmailMessage()
+    email_message["Subject"] = "new customar showed up!"
+    email_message.set_content("Hey, we just see a new customar!")
+
+    with open (image_path,"rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None,content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)    
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string() )
+    gmail.quit()
+
+if __name__ =="__main__":
+    send_email(image_path="venv/images/15.png")    
